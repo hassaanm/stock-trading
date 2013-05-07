a2be4d1bd2c83e5c7acffc8a970abee08d44ea50
hassaanm <hassaanmarkhiani@gmail.com>
2013-05-07 06:52:17 -0500

    Fixed testing set in portfolio

--- src/core/rl/portfolio.py
+++ src/core/rl/portfolio.py
@@ -10,7 +10,7 @@ class Portfolio(object):
     def __init__(self, testSize=0.2, pickNum=5, money=10000.0, verbose = False):
         self.stockHistory = StockHistory('nasdaq100')
         self.companies = self.stockHistory.compNames()
-        self.featurizer = Featurizer(self.stockHistory)
+        self.featurizer = Featurizer(self.stockHistory, 5, 5, 5, 0, 1.5, -1.5)
         self.numberOfFeatures = self.featurizer.numFeatures
 
         self.A = dict()
@@ -54,7 +54,7 @@ class Portfolio(object):
         date = startDate
         # real, random and average money
         money = [self.startMoney, self.startMoney, self.startMoney]
-        testStartDate = timedelta(days=int((endDate - date).days * self.testPercentage)) + date
+        testStartDate = timedelta(days=int((endDate - date).days * (1 - self.testPercentage))) + date
         while date < endDate :
             companies = [company for company in self.companies if self.featurizer.isValidDate(company, date)]
             features = [self.featurizer.getFeatures(company, date) for company in companies]
@@ -62,7 +62,7 @@ class Portfolio(object):
             if len(companies) > 0 :
                 stocks, moneyReturns, sharpeRatio = self.LinUCB(companies, features, returns)
                 if self.verbose:
-                    print ('%8.5f\t%8.5f\t%8.5f\t%8.5f\t%s') % (moneyReturns[0], moneyReturns[1], moneyReturns[2], sharpeRatio, stocks)
+                    print ('%10f\t%8.5f\t%8.5f\t%8.5f\t%8.5f\t%s') % (money[0], moneyReturns[0], moneyReturns[1], moneyReturns[2], sharpeRatio, stocks)
                 if date > testStartDate:
                     openPrices = {company: self.stockHistory.get(company, date, OPEN) for company in stocks}
                     cost = self.getTradeCost(money[0], stocks, openPrices)
@@ -72,7 +72,9 @@ class Portfolio(object):
 
     def LinUCB(self, stocks, features, rewards):
         p = []
+        print '*' * 100
         for stock, feature, reward in zip(stocks, features, rewards):
+            print ('%s %5s %10.7f') % (feature, stock, reward)
             if stock not in self.A:
                 self.A[stock] = numpy.identity(self.numberOfFeatures)
                 self.b[stock] = numpy.zeros(self.numberOfFeatures)
@@ -81,6 +83,7 @@ class Portfolio(object):
             p.append(float(numpy.dot(theta.T, x) + self.alpha * math.sqrt(numpy.dot(numpy.dot(x.T, numpy.linalg.inv(self.A[stock])), x))))
             self.A[stock] += numpy.dot(x, x.T)
             self.b[stock] += reward * x.T
+        print '*' * 100
 
         chosen = []
         sortedStocks = numpy.argsort(p)[::-1]
